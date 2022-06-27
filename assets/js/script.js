5be7c074199799b3300e8089dfef40c555e46c53
qosole <57912371+qosole@users.noreply.github.com>
2022-06-27 12:22:17 -0500

    Fix bug with start button and add some comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var startButton = document.getElementById("start-button");
 var quizChoices = document.getElementsByClassName("quiz-choices");
 
+// Changing the page when user presses "START!" button
 function startQuiz() {
     for (var i = 0; i < 4; i++) {
         quizChoices[i].style.display = "block";
