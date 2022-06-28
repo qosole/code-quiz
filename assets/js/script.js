3b2d0e934b6f950aa59914db45d227a48079f3d5
qosole <williamj99wei@gmail.com>
2022-06-27 19:45:12 -0500

    Fix bugs to UI when buttons are clicked; write second question; clean up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,43 @@ var topLine = document.getElementById("top-line");
 var correct = document.getElementById("correct-incorrect");
 
 var choicesNumber = 4; // Quiz will be multiple choice with 4 choices
+var delay = 1000; // 1 second delay
+
+function secondQuestion() {
+    // Removing the result of the last question after delay (1 second)
+    var resultTimer = setTimeout(function() {
+        correct.textContent = "";
+    }, delay);
+
+    // Changing page to display second question
+    quizQuestion.textContent = "Which is NOT a data type?";
+    quizGuide.style.display = "none";
+    topLine.style.width = "70%";
+    quizChoices[0].textContent = "string";
+    quizChoices[1].textContent = "number";
+    quizChoices[2].textContent = "array";
+    quizChoices[3].textContent = "farley";
+
+    // Adding functionality to quiz choices buttons (farley is correct)
+    for (var i = 0; i < choicesNumber; i++) {
+        quizChoices[i].addEventListener("click", function(event) {
+            var userChoice = event.target;
+            if (userChoice.textContent == "farley") {
+                correct.textContent = "Correct! :D";
+                correct.style.color = "green";
+                clearTimeout(resultTimer); // Need to clear timer so that the first result timer does not bleed into second result timer
+            } else {
+                correct.textContent = "Incorrect :(";
+                correct.style.color = "red";
+                clearTimeout(resultTimer);
+            }
+        });
+    }
+}
 
 function firstQuestion() {
     // Changing page to display first question
-    quizQuestion.textContent = "What keyword do you use to initialize a variable with a value of 55.2?";
+    quizQuestion.textContent = "What keyword would you use to initialize a variable with a value of 55.2?";
     quizGuide.style.display = "none";
     topLine.style.width = "70%";
     quizChoices[0].textContent = "string";
@@ -24,9 +57,11 @@ function firstQuestion() {
             if (userChoice.textContent == "var") {
                 correct.textContent = "Correct! :D";
                 correct.style.color = "green";
+                secondQuestion(); // Proceeding to next question
             } else {
                 correct.textContent = "Incorrect :(";
                 correct.style.color = "red";
+                secondQuestion(); 
             }
         });
     }
@@ -39,7 +74,7 @@ function startQuiz() {
         quizChoices[i].style.display = "block";
     }
     startButton.style.display = "none";  // Hide start button
-    firstQuestion(); // Proceeding to the first question
+    firstQuestion(); // Proceeding to first question
 }
 
 startButton.addEventListener("click", startQuiz);
