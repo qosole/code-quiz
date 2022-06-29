c8c938660ad173c2bc66df763428038f3bd1a7b2
qosole <williamj99wei@gmail.com>
2022-06-28 19:58:40 -0500

    Add timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,14 @@ var quizGuide = document.getElementById("guide");
 var topLine = document.getElementById("top-line");
 var correct = document.getElementById("correct-incorrect");
 var timerDisplay = document.getElementById("timer");
+var resetButton = document.querySelector(".reset-score");
 
 var choicesNumber = 4; // Quiz will be multiple choice with 4 choices
 var delay = 1000; // 1 second delay
 var score = 0; // User's quiz score
 var timer = 60000; // How much time user has to take the quiz in milliseconds
 var timeRemaining = 60; // How much time user has to take the quiz in seconds
-var quizTimer;
+var quizTimer; // Used to stop the quiz timer
 
 function showHighScores() {
     // Changing page to display high scores (stored in local storage)
@@ -36,6 +37,9 @@ function showHighScores() {
 
     scoresList.appendChild(scoresDisplay);
     document.body.appendChild(scoresList);
+
+    // Button to reset score
+    resetButton.style.display = "block";
 }
 
 function endQuiz() {
@@ -46,10 +50,12 @@ function endQuiz() {
 
     // Stopping the timer
     clearInterval(quizTimer);
+    timerDisplay.style.display = "none";
 
     // Changing page to display results
     quizQuestion.textContent = "FINISH!";
-    quizGuide.style.display = "block";;
+    quizGuide.style.display = "block";
+    score += timeRemaining;
     quizGuide.textContent = "Your score: " + score; 
     topLine.style.width = "0%";
     for (var i = 0; i < choicesNumber; i++) {
@@ -316,10 +322,14 @@ function firstQuestion() {
 function startTimer() {
     // Setting the timer on screen to the delay in seconds
     timerDisplay.textContent = timeRemaining;
-    timeRemaining--;
 
     // Ending the quiz when time reaches 0
-
+    if (timeRemaining <= 0) {
+        correct.textContent = "Out of time?!";
+        correct.style.color = "cyan";
+        endQuiz();
+    }
+    timeRemaining--;
 }
 
 // Hiding "START!" button and revealing quiz choices when user presses "START!" button
